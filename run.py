0d9a0275cbb06e4fe0eedcfd34310e1b281913f9
Ken Newcomb <KenNewcombNJ@gmail.com>
2015-02-17 19:52:17 -0500

    First commit

---
+++ run.py
@@ -0,0 +1,7 @@
+### WikiDFS: Performs a depth-first-search on a wikipedia entry to find a keyword.
+
+# Basic Algorithm:
+
+# Parse user input (run.py microsoft https://en.wikipedia.org/Apple)
+
+# Scrape wikipedia entry text for all links.
